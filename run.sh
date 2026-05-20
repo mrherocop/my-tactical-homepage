2a7be0745e24ef4c79692f62caff79f1554e6b71
mrherocop <herocop3.0@gmail.com>
2026-05-20 10:32:31 +0530

    Removed run.sh script

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Start a simple background server for the Firefox New Tab Page
-cd /home/sayan-pal/Desktop/firefox_homepage
-nohup python3 -m http.server 8080 > /dev/null 2>&1 &
-echo "Server started on http://localhost:8080"
